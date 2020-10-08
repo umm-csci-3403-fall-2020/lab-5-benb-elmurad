99016e64328ffa4e0ad055f065b83caf06dc211a
Ben <burge337@morris.umn.edu>
2020-10-08 12:08:44 -0500

    Initial work for creating the client java code
    Doesn't compile properly right now.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,45 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) throws IOException {
+		String server;
+		// bother localhost if we aren't given a server
+		if (args.length == 0) {
+			server = "127.0.0.1";
+		} else {
+			server = args[0];
+		}
+
+		try {
+			// connect to server
+			Socket socket = new Socket(server, portNumber);
+			
+			OutputStream output = socket.getOutputStream();
+			while (System.in.read() != null) {
+				output.write(System.in.read());
+			}
+			
+
+			// Grab input stream so we can read from it
+			InputStream input = socket.getInputStream();
+			while (input.available() > 0) {
+				System.out(input.read());
+			}
+
+			// close socket when we're done with it
+			socket.close();
+
+		} catch (ConnectException ce) {
+			System.out.println("We were unable to connect to " + server);
+			System.out.println("You should make sure the server is running.");
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
+	}
+			
 }
