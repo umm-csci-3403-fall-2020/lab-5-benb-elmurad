e708dc3a28a846792adea652453364b3474946fc
Ben <burge337@morris.umn.edu>
2020-10-13 12:26:17 -0500

    Finish implementation of EchoClient.java
    It seems like splitting the functionality of the program into two separate while loops was doing really terrible
    things, with outputs seemingly just randomly stopping after a line or two.
    
    Using a single while loop made this much easier (and it may have even been impossible with the two while loops)
    to send a single byte at a time, as we could write it, send it, receive it, and write to the system all in the same loop.
    
    Also added close() statements for the socket and it's input/output streams at the end.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -16,23 +16,30 @@ public class EchoClient {
 
 		try {
 			// connect to server
+
+			// create our socket and input/output streams to it
 			Socket socket = new Socket(server, portNumber);
-			
-			OutputStream output = socket.getOutputStream();
-			while (System.in.read() != null) {
-				output.write(System.in.read());
-			}
-			
-
-			// Grab input stream so we can read from it
 			InputStream input = socket.getInputStream();
-			while (input.available() > 0) {
-				System.out(input.read());
+			OutputStream output = socket.getOutputStream();
+
+			int echoThisByte; // integer to hold the byte we will be tossing around
+			// System.in.read() returns -1 when the steam is done
+			while ((echoThisByte = System.in.read()) != -1) {
+				// write our byte to the outputstream
+				output.write(echoThisByte);
+				// send it off to the server
+				output.flush();
+				// receive byte from server and write to System.out
+				System.out.write(input.read());
+				// jpg test failing without this => flush the system output so that it doesn't try to write groups of bytes and mess everything up
+				System.out.flush();
 			}
-
-			// close socket when we're done with it
+			// Close all the things we opened now that we're done
 			socket.close();
-
+			input.close();
+			output.close();
+			
+			// very basic error handling
 		} catch (ConnectException ce) {
 			System.out.println("We were unable to connect to " + server);
 			System.out.println("You should make sure the server is running.");
