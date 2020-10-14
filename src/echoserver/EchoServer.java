569285a7ad9087c61ec47654311cf6fa8a29d487
Murad Abbasov <Murad@mrs-er-10-133-58-117.wireless.umn.edu>
2020-10-14 15:09:28 -0500

    Add EchoServer.java

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,38 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = sock.accept();
+        System.out.println("Got a request!");
+
+	    int Bytes;
+	    InputStream input = client.getInputStream();
+	    OutputStream output = client.getOutputStream();
+
+	    while ((Bytes = input.read()) != -1){
+	      output.write(Bytes);
+        }
+
+        client.close();
+      }
+
+    // Minimal error handling
+
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
